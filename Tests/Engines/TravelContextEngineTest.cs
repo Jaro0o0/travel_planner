04a1458a9141a0cbd3063cf2e771a1cfd741e189
Jaro0o0 <jar0dev80@gmail.com>
2026-08-30 10:50:54 +0200

    add tests

--- Tests/Engines/TravelContextEngineTest.cs
+++ Tests/Engines/TravelContextEngineTest.cs
@@ -1,21 +1,51 @@
-using Services;
+using ShareTravelPalace;
 using Xunit;
-public  class TravelContextEngineTests{
-    
-   
+
+public class TravelContextEngineTests
+{
+    //Rainy_Weather_Test
+    [Fact]
     public void RainyWeatherTest()
     {
-         //Arange
-        var service
-        var travelContext = new TravelContextEngine();
-        var place = new Place();
+        // Arrange
+        var engine = new ContextEngine();
+        var place = new Place
+        {
+            Types = new List<string> { "park","beach","ice_cream_shop" }
+        };
+        var context = new TripContext
+        {
+            WeatherCondition = "rain"
+        };
 
-        //Act
-        var score =  travelPlan 
+        // Act
+        var score = engine.ScorePlace(place, context);
 
         // Assert
-        Assert.True( score < 0 );
-         
+        Assert.True(score < 0);
     }
-   
+
+    //Sunny_Weather_Test
+    [Fact]
+    public void SunnyWeatherTest()
+    {
+         // Arrange
+        var engine = new ContextEngine();
+        var place = new Place
+        {
+            Types = new List<string> { "park","beach","ice_cream_shop"   }
+        };
+        var context = new TripContext
+        {
+            WeatherCondition = "sunny",
+            Temperature = 30
+        };
+
+         // Act
+        var score = engine.ScorePlace(place, context);
+
+        // Assert
+        Assert.True(score > 0);
+
+    }
 }
